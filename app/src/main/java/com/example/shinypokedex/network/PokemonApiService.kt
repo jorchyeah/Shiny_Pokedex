44703b88463a782e4fcb9e16e630301665459f4d
jorchyeah <jc.3@live.com.mx>
2020-10-30 16:31:21 -0600

    Retrofit interface was added

---
+++ app/src/main/java/com/example/shinypokedex/network/PokemonApiService.kt
@@ -0,0 +1,25 @@
+package com.example.shinypokedex.network
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private val BASE_URL = "https://pokeapi.co/api/v2/"
+
+private val retrofit = Retrofit
+    .Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface PokemonApiService{
+    @GET("pokemon")
+    fun getPokemon(): Call<String>
+
+    object PokemonApi{
+        val retrofitService: PokemonApiService by lazy {
+            retrofit.create(PokemonApiService::class.java)
+        }
+    }
+}
