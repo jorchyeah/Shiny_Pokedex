b389f9034334d4c1e31e0a524cb14f811f7a6eed
jorchyeah <jc.3@live.com.mx>
2020-10-30 16:42:19 -0600

    ViewModel for the overview fragment was added

--- app/src/main/java/com/example/shinypokedex/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/shinypokedex/overview/OverviewFragment.kt
@@ -6,21 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.shinypokedex.R
 import com.example.shinypokedex.databinding.FragmentOverviewBinding
 
 class OverviewFragment : Fragment() {
+
+    private val viewModel: OverviewViewModel by lazy {
+        ViewModelProvider(this).get(OverviewViewModel::class.java)
+    }
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         val binding = DataBindingUtil.inflate<FragmentOverviewBinding>(
-                inflater,
-                R.layout.fragment_overview,
-                container,
-                false
+            inflater,
+            R.layout.fragment_overview,
+            container,
+            false
         )
+
+        binding.lifecycleOwner = this
+
+        binding.viewModel = viewModel
+
         return binding.root
     }
 }
